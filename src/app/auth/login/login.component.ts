bc80030c1f4dabfd5d7e29facc13e12578d5d34c
Chetan Kulkarni <son.chetanakulkarni@gmail.com>
2019-11-16 07:30:36 +0530

    Called Login API

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ILoginModel, LoginModel, ResponseModel } from './login.model';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
   }
+
+  login() {
+
+    const loginModel: LoginModel = new LoginModel('son.chetanakulkarni@gmail.com', 'Chetan@123');
+    this.loginService.login(loginModel).subscribe((response: ResponseModel) => {
+      console.log(response.data);
+    });
+  }
+
 
 }
