f5ac29a2bc473865c8cf78320b91645225a4daef
Nikhil Jadhav <nikhil.jadhav1527@gmail.com>
2020-02-12 22:45:10 +0530

    change in design.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+declare var $: any;
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  openLogoutModal() {
+    $(".ui.basic.modal").modal("show");
   }
 
+  Logout() {
+    this.router.navigateByUrl("");
+  }
 }
